7b318bc174d094ee096f24eb123a70b94431baf5
Akkarz <151856896+Akkarz@users.noreply.github.com>
2024-02-03 10:20:35 +0300

    Create sdgjh.js

---
+++ scripts/sdgjh.js
@@ -0,0 +1,16 @@
+detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function() {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
